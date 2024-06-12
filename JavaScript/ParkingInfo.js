5c667dab39f4179643828c8c079a45efb27c8965
LeeSangHyeok <140476089+KaneBigNose@users.noreply.github.com>
2024-06-12 16:14:56 +0900

    복구

--- JavaScript/ParkingInfo.js
+++ JavaScript/ParkingInfo.js
@@ -1,55 +1,52 @@
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const bottomSheet = document.getElementById('bottomSheet');
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
 
 var prkOnclick = false;
 
 function toggleSelect() {
-    var selectContainer = document.querySelector('.box1 select');
-    if (selectContainer) {
-        selectContainer.remove();
-    }
-    else {
-        var select = document.createElement('select');
-        select.name = "filter";
-        select.id = "select";
-        select.innerHTML = `
+  var selectContainer = document.querySelector(".box1 select");
+  if (selectContainer) {
+    selectContainer.remove();
+  } 
+  else {
+    makeSelectBox();
+  }
+}
+
+function makeSelectBox() {
+  var select = document.createElement("select");
+  select.name = "filter";
+  select.innerHTML = `
             <option value="1" selected>거리순</option>
             <option value="2">요금순</option>`;
-        document.querySelector('.box1').appendChild(select);
-
-        select.addEventListener("touchstart", function(event) {
-            event.stopPropagation(); // 부모 요소의 클릭 이벤트 전파 방지
-        });
-    }
+  box1.appendChild(select);
 }
 
 function toggleParking() {
-    if (prkOnclick == false) {
-        addMarkersFromJson(jsonFilePath);
-        addSubTitleBox2();
-        toggleSelect();
-        prkOnclick = true;
-    }
-    else {
-        clearMarkers();
-        clearBox2();
-        prkOnclick = false;
-    }
+  if (prkOnclick == false) {
+    addMarkersFromJson(jsonFilePath);
+    addSubTitleBox2();
+    toggleSelect();
+    prkOnclick = true;
+  } else {
+    clearMarkers();
+    clearBox2();
+    prkOnclick = false;
+  }
 }
 
 function clearBox2() {
-    while (box1.firstChild) {
-        box1.removeChild(box1.firstChild);
-    }
-    while (box2.firstChild) {
-        box2.removeChild(box2.firstChild);
-    }
+  while (box1.firstChild) {
+    box1.removeChild(box1.firstChild);
+  }
+  while (box2.firstChild) {
+    box2.removeChild(box2.firstChild);
+  }
 }
 
 function addSubTitleBox2() {
-    const newBox = document.createElement('div');
-    newBox.className = 'subtitle-box';
-    newBox.textContent = `주차장 정보`;
-    box1.appendChild(newBox);
+  const newBox = document.createElement("div");
+  newBox.className = "subtitle-box";
+  newBox.textContent = `주차장 정보`;
+  box1.appendChild(newBox);
 }
