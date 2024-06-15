f92275e10971bf521f881a96dcc34898fe16cbcb
lemon1325 <er007345@gmail.com>
2024-06-15 14:59:23 +0900

    css 수정

--- JavaScript/ParkingInfo.js
+++ JavaScript/ParkingInfo.js
@@ -3,34 +3,12 @@ const box2 = document.querySelector(".box2");
 
 var prkOnclick = false;
 
-function toggleSelect() {
-  var selectContainer = document.querySelector(".box1 select");
-  if (selectContainer) {
-    selectContainer.remove();
-  } 
-  else {
-    makeSelectBox();
-  }
-}
-
-function makeSelectBox() {
-  var select = document.createElement("select");
-  select.name = "filter";
-  select.innerHTML = `
-            <option value="1" selected>거리순</option>
-            <option value="2">요금순</option>`;
-  box1.appendChild(select);
-}
-
 function toggleParking() {
   if (prkOnclick == false) {
     addMarkersFromJson(jsonFilePath);
-    addSubTitleBox2();
-    toggleSelect();
     prkOnclick = true;
   } else {
     clearMarkers();
-    clearBox2();
     prkOnclick = false;
   }
 }
@@ -42,11 +20,4 @@ function clearBox2() {
   while (box2.firstChild) {
     box2.removeChild(box2.firstChild);
   }
-}
-
-function addSubTitleBox2() {
-  const newBox = document.createElement("div");
-  newBox.className = "subtitle-box";
-  newBox.textContent = `주차장 정보`;
-  box1.appendChild(newBox);
 }
