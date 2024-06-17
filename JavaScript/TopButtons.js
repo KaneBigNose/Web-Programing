be393266900541ba8284a6728be8fd241c5d4db8
KaneBigNose <imddongs@naver.com>
2024-06-17 20:59:10 +0900

    필요없는 기능 삭제

--- JavaScript/TopButtons.js
+++ JavaScript/TopButtons.js
@@ -1,18 +1,3 @@
-
-function openWindowAndDisableButton() {
-    var button = document.getElementById('navBtn');
-    button.disabled = true;
-
-    var newWindow = window.open('https://map.kakao.com/link/to/35.13457,129.1031', '_blank');
-
-    var checkWindowClosed = setInterval(function () {
-        if (newWindow.closed) {
-            clearInterval(checkWindowClosed);
-            button.disabled = false;
-        }
-    }, 1000);
-}
-
 function toggleButtons() {
     var buttons = ['illegalBtn', 'parkingBtn', 'clearBtn'];
     buttons.forEach(id => {
